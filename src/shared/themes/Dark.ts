e04ccf4950ad64629fe1ce81122f973fef903a8d
sergiocouter <sergiocouter@gmail.com>
2023-06-14 15:33:13 -0300

    Adicionado tema escuro e troca de tema

---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+import { deepPurple, yellow } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+    palette:{
+        primary:{
+            main: deepPurple[500],
+            dark: deepPurple[900],
+            light: deepPurple[200],
+            contrastText: '#FFFFFF',
+        },
+        secondary:{
+            main: yellow[500],
+            dark: yellow[800],
+            light: yellow[300],
+            contrastText: '#FFFFFF',
+        },
+        background:{
+            default:'#202124',
+            paper:'#303134',
+        }
+    }
+})
