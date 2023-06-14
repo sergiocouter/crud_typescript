c742d117d861ac93ea36ba71af772eb6c50bd1b0
sergiocouter <sergiocouter@gmail.com>
2023-06-13 23:14:49 -0300

    Foi adicionado o react-router-dom@6

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
