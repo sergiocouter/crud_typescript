0fc409c170e8292539ddb867b919e556b7c1903c
sergiocouter <sergiocouter@gmail.com>
2023-06-17 10:17:03 -0300

    Adicionado responsividade no menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,7 +1,8 @@
-import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material";
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import logo from "./../../../assets/logo-crud-tp.png";
+import { useDrawerContext } from "../../contexts";
 
 interface IMenuLateralProps{
     children: React.ReactNode
@@ -10,10 +11,12 @@ interface IMenuLateralProps{
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
 
   const theme = useTheme();  
+  const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const {isDrawerOpen, toggleDrawerOpen} = useDrawerContext();
 
   return(
     <>
-      <Drawer variant="permanent">
+      <Drawer open={isDrawerOpen} variant={smDown ? "temporary" : "permanent"} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column" >
 
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
@@ -43,7 +46,7 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
         </Box>   
       </Drawer>
 
-      <Box height="100vh" marginLeft={theme.spacing(28)}>
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
     </>
---
+++ src/shared/contexts/DrawerContext.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState, useCallback, useContext } from "react";
+
+
+interface IDrawerContextData{
+    isDrawerOpen: boolean;
+    toggleDrawerOpen: () => void;
+}
+
+const DrawerContext = createContext({} as IDrawerContextData);
+
+export const useDrawerContext = () =>{
+  return useContext(DrawerContext);
+};
+
+export const DrawerProvider: React.FC<{children: React.ReactNode}> = ({ children }) =>{
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawerOpen = useCallback( () => {
+    setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen);
+  },[]);
+
+  return(
+    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen}}>
+      {children}
+    </DrawerContext.Provider>
+  );
+};
