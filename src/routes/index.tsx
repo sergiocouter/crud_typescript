ac962094706e82c5a1c13cf18ccc4bc2679b8310
sergiocouter <sergiocouter@gmail.com>
2023-06-28 22:29:30 -0300

    Criando página do Dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@mui/material";
 import { Routes, Route, Navigate} from "react-router-dom";
 import { useDrawerContext } from "../shared/contexts";
 import { useEffect} from "react";
+import { Dashboard } from "../pages";
 
 export const AppRoutes = () =>{
 
-  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
 
   useEffect(() => {
     setDrawerOptions([
@@ -21,10 +21,7 @@ export const AppRoutes = () =>{
     <Routes>
       <Route path="/pagina-inicial" 
         element={
-          <Button variant="contained" 
-            color="primary" 
-            onClick={toggleDrawerOpen}>Toggle Drawer
-          </Button>
+          <Dashboard />
         } 
       />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
---
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/system";
+
+export const LayoutBaseDePagina: React.FC = ({ children }) => {
+
+  return(
+    <Box>
+      teste
+      {children}
+    </Box>
+  );
+};
